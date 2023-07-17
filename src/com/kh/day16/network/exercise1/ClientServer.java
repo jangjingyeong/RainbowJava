2bec785915255373beed019a59431462326ffbff
jangjingyeong <jjg070593@gmail.com>
2023-07-17 17:28:31 +0900

    [ADD] 23/07/17 Day16 네트워크 프로글밍 개념 및 실습
    1. TCP, IP, Port 개념 설명
    2. Socket 개념 설명 및 실습
    3. Socket을 이용한 채팅 프로그램(Server, Client) 구현

---
+++ src/com/kh/day16/network/exercise1/ClientServer.java
@@ -0,0 +1,51 @@
+package com.kh.day16.network.exercise1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class ClientServer {
+	public static void main(String[] args) {
+		String adress = "192.168.60.223";
+		int port = 7777;
+		
+		InputStream is = null;
+		OutputStream os = null;
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("연결 요청중...");
+			// 설정된 IP, PORT에 연결 요청함.
+			Socket socket = new Socket(adress, port);
+			// 연결된 후에는 소켓 객체 생성
+			System.out.println("연결 성공!!");
+			os = socket.getOutputStream();	// 반복될 필요 없음
+			// 프로그램 기준 나가니까 OutputStream
+			is = socket.getInputStream();
+			// 프로그램 기준 들어오니까 InputStream
+			
+			while(true) {
+				// ================ 1. 데이터 보내기 ================
+				System.out.print("클라이언트(나) : ");	// 가이드 메시지
+				String message = sc.nextLine();			// 메시지 입력받음
+				byte [] bytes = message.getBytes();		// 메시지 배열에 저장
+				// 보낼때 버퍼에 씀 write() 메소드 사용 
+				os.write(bytes); 
+				// 버퍼 비워주기 flush()! 
+				os.flush();
+				// ================ 4. 데이터 받기 ================
+				bytes = new byte[1024];
+				int readByteNo = is.read(bytes);
+				message = new String(bytes, 0, readByteNo);
+				System.out.printf("서버(상대) : %s\n", message);
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
